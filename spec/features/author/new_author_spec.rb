e7fcba5e424db5df01223f9d15c44139d29973e7
Matthis Clausen <matthis.clausen@student.hpi.de>
2022-10-25 14:32:33 +0200

    Test and working Code for Issue 2

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 describe "New author page", type: :feature do
-  it "should exist at 'new_author_path' and render withour error" do
-    #https://guides.rubyonrails.org/routing.html#path-and-url-helpers
-    visit new_author_path
-  end
+    it "should exist at 'new_author_path' and render withour error" do
+        #https://guides.rubyonrails.org/routing.html#path-and-url-helpers
+        visit new_author_path
+    end
+    it "should have input fields to input a new authors first name, last name , and homepage" do
+        visit new_author_path
+        expect(page).to have_field('author[first_name]')
+        expect(page).to have_field('author[last_name]')
+        expect(page).to have_field('author[homepage]')
+    end
 end
