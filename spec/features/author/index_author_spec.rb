fa9e8829a271305e21b9f0abacdc918fa1258859
Matthis Clausen <matthis.clausen@student.hpi.de>
2022-10-30 11:23:25 +0100

    Solves Issue 7

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -4,6 +4,10 @@ describe "Author index page", type: :feature do
     it "should render without error" do
         visit authors_path
     end
+    it "should have a table with a row for each author" do
+        visit authors_path
+        expect(page).to have_css('table#authors tr', count: Author.count+1)
+    end
     it "should have a link to the new author page" do
         visit authors_path
         expect(page).to have_link('Add author', href: new_author_path)
