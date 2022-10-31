40b40d35743ff548660e93343f6d26b58342c910
Matthis Clausen <matthis.clausen@student.hpi.de>
2022-10-31 14:23:10 +0100

    Solves Issue 17

--- spec/features/paper/index_paper_spec.rb
+++ spec/features/paper/index_paper_spec.rb
@@ -8,4 +8,10 @@ it "should have a link to eeach paper's edit page" do
       expect(page).to have_link('Edit', href: edit_paper_path(paper))
     end
   end
+it "should have a link to delete a paper" do
+    visit papers_path
+    Paper.all.each do |paper|
+      expect(page).to have_link('Delete', href: paper_path(paper))
+    end
+  end
 end
