c0ef7886453eab50e543d30e369feacb680b5df1
Matthis Clausen <matthis.clausen@student.hpi.de>
2022-11-04 15:30:46 +0100

    Last Issue?
    resolves: #21

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -4,6 +4,7 @@ class PapersController < ApplicationController
   # GET /papers
   def index
     @papers = Paper.all
+    @papers = @papers.where("year = ?", params[:year]) if params[:year].present?
   end
 
   # GET /papers/1
